d96617baf85f576d50cd98bba2b3886e039bea24
Molnija3D <molnija3d@yandex.ru>
2024-04-06 20:46:01 +0300

    set locale ru_RU.utf8

--- hw_01.c
+++ hw_01.c
@@ -3,15 +3,15 @@
 
  int main(void)
  {
-     setlocale(LC_ALL, "Rus");
-     printf("Задание №1:\n");
+     setlocale(LC_ALL, "ru_RU.utf8");
+     printf("Задание #1:\n");
      printf("Hello world!\n\n");
-     printf("Задание №2:\n");
+     printf("Задание #3:\n");
      printf("лесенкой\n");
      printf("      Let's\n");
      printf("      go\n");
      printf("                  to walk\n\n");
-     printf("Задание №3:\n");
+     printf("Задание #3:\n");
      printf("   *\n");
      printf("  ***\n");
      printf(" *****\n");

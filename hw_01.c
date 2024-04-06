0c61273799acb5cbe1a147f74389d1b7c97dab8c
Molnija3D <molnija3d@yandex.ru>
2024-04-06 20:59:49 +0300

    Fix task numbers

--- hw_01.c
+++ hw_01.c
@@ -6,7 +6,7 @@
      setlocale(LC_ALL, "ru_RU.utf8");
      printf("Задание #1:\n");
      printf("Hello world!\n\n");
-     printf("Задание #3:\n");
+     printf("Задание #2:\n");
      printf("лесенкой\n");
      printf("      Let's\n");
      printf("      go\n");

9d2ea97fd83793cfc2fd860c8d6cc57d55087a89
Molnija3D <molnija3d@yandex.ru>
2024-04-06 20:36:27 +0300

    add hw_01.c

---
+++ hw_01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+ int main(void)
+ {
+     setlocale(LC_ALL, "Rus");
+     printf("Задание №1:\n");
+     printf("Hello world!\n\n");
+     printf("Задание №2:\n");
+     printf("лесенкой\n");
+     printf("      Let's\n");
+     printf("      go\n");
+     printf("                  to walk\n\n");
+     printf("Задание №3:\n");
+     printf("   *\n");
+     printf("  ***\n");
+     printf(" *****\n");
+     printf("*******\n");
+     printf(" HH HH\n");
+     printf(" ZZZZZ\n");
+ return 0;
+ }

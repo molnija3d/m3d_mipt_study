33a141c2006158ad3698a696e89119101da1af89
Molnija3D <molnija3d@yandex.ru>
2024-04-07 20:29:03 +0300

    adapt for autotest

--- hw_02.c
+++ hw_02.c
@@ -4,6 +4,8 @@
  int main(void)
  {
      setlocale(LC_ALL, "ru_RU.utf8");
-     printf("Hello world!\n");
+     printf("      Let's\n");
+     printf("         go\n");
+     printf("             to walk\n\n");
  return 0;
  }
---
+++ hw_03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+ int main(void)
+ {
+     setlocale(LC_ALL, "ru_RU.utf8");
+     printf("   *\n");
+     printf("  ***\n");
+     printf(" *****\n");
+     printf("*******\n");
+     printf(" HHOHH\n");
+     printf(" ZZZZZ\n");
+ return 0;
+ }

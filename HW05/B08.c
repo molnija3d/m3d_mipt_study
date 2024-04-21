141754bf1ef69e722f45b54b9114c4e09aa81d27
Molnija3D <molnija3d@yandex.ru>
2024-04-21 09:26:15 +0300

    Добавить B08.c

---
+++ HW05/B08.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+
+int main(void)
+{
+    int32_t a;
+    scanf("%"SCNd32,&a);
+    do
+    {
+        if((a%10)%2)
+        {
+          printf("NO\n");
+          return 0;  
+        }
+    }
+    while(a/=10);
+    printf("YES\n");
+
+    return 0;
+}

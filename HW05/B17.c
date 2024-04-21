8debb3a22c145889507a5185f54e842cb6aebf0e
Molnija3D <molnija3d@yandex.ru>
2024-04-21 12:36:02 +0300

    Добавить B17.c

---
+++ HW05/B17.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int32_t sum(int32_t a);
+int32_t mul(int32_t a);
+
+int main(void)
+{
+    int32_t a,tmp = 9;
+
+    scanf("%"SCNd32,&a);
+
+    while (tmp++ < a)
+        if(sum(tmp) == mul(tmp))
+           printf("%"PRId32" ",tmp);
+    
+    return 0;
+}
+int32_t sum(int32_t a)
+{
+  int32_t sum=0;
+  do
+  {
+    sum += a%10;  
+  } 
+  while (a/=10);
+  return sum;  
+}
+
+int32_t mul(int32_t a)
+{
+  int32_t mul=1;
+  do
+  {
+    mul *= a%10;  
+  } 
+  while (a/=10);
+  return mul;    
+}

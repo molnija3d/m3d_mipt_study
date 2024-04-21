8f524313837feb30962c26ab041bcf70eaccfd34
Molnija3D <molnija3d@yandex.ru>
2024-04-21 08:52:40 +0300

    Добавить B07.c

---
+++ HW05/B07.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+
+int main(void)
+{
+    int32_t a,d1,d2;
+    scanf("%"SCNd32,&a);
+    d1 = a%10;
+    while(a/=10)
+    {
+     d2 = a%10;
+     if (d1 == d2) 
+     {
+         printf("YES\n");
+         return 0;
+     }
+    }
+    printf("NO\n");    
+    return 0;
+}

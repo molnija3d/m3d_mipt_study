3521dad83cf02b9a1932ead8477f6d8fb2508b59
Molnija3D <molnija3d@yandex.ru>
2024-04-22 11:00:52 +0300

    Изменить B21.c

--- HW05/B21.c
+++ HW05/B21.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-#include <inttypes.h>
-
 
 int main(void)
 {
-    unsigned char c,s [100];
-    int32_t i=0;
+    unsigned char c;
     while ((c = getchar())!= '.')
     {
        if(c > 64 && c < 91) 
-          c += 32;   
-       if(c!='.') 
-          s[i++] = c;
+          c += 32;
+       putchar(c);
     }
-    s[i]='\0';
-    printf("%s",s);
     
     return 0;
 }

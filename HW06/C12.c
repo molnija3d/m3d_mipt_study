c3b72d77516b55745d78469568a133f7774bba3d
Molnija3D <molnija3d@yandex.ru>
2024-04-23 18:45:54 +0300

    Добавить C13.c, поправить С12.c

--- HW06/C12.c
+++ HW06/C12.c
@@ -8,24 +8,22 @@ int main(void)
     float x;
     scanf("%f",&x);
     x *= 3.141592 / 180.;
-    printf("%.3f",sinus(x));
+    printf("%.3f\n",sinus(x));
     return 0;
 }
 
 /*sin(x) = x - x3/3! + x5/5! - x7/7 + ...*/
 float sinus(float x)
 {
-    float sin=0.f;
-    float x_x = x*x;
+    float sin=0.f, fact = 1.0f, x_x = x*x;
     float p_x = x;
-    float fact = 1.0f;
     int sign=1;
     for(int32_t i=1;i<8;i+=2)
     {
      fact /= i;
      sin += sign*p_x*fact;
      fact /= (i+1);
-     sign = -sign;
+     sign = -sign; 
      p_x *= x_x;
     }
     return sin;

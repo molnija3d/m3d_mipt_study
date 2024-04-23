b179cff92f295b40ab2217f401ee58eaa9209161
Molnija3D <molnija3d@yandex.ru>
2024-04-23 13:23:05 +0300

    Добавить C01.c

---
+++ HW06/С01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int32_t module(int32_t a);
+
+int main(void)
+{
+    int32_t a;
+    
+    scanf("%"SCNd32,&a);
+    printf("%"PRId32"\n",module(a));
+    
+    return 0;
+}
+
+int32_t module(int32_t a)
+{
+    if (a > 0) return a;
+    return -a;
+}

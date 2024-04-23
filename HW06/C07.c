8f4443b0b08c90799c4859dea4d171dbb6909a91
Molnija3D <molnija3d@yandex.ru>
2024-04-23 16:09:04 +0300

    Добавить C07.c

---
+++ HW06/C07.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int32_t conv(int32_t n,int32_t p);
+
+int main(void)
+{
+    int32_t n,p;
+     
+    scanf("%"SCNd32" %"SCNd32,&n,&p);
+    printf("%"PRId32"\n",conv(n,p)) ;
+    return 0;
+}
+
+int32_t conv(int32_t n, int32_t p)
+{
+    uint32_t res = 0;
+    for(uint32_t i = 1;n;i*=10, n/=p)
+    {
+        res += n%p*i;
+    }
+    return  res;
+}

1e317e1894f53ae883a22639fd73fcb1720ffa08
Molnija3D <molnija3d@yandex.ru>
2024-04-23 16:33:33 +0300

    Добавить C9.c

---
+++ HW06/C09.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int32_t factorial(int32_t n);
+
+int main(void)
+{
+    int32_t n;
+    scanf("%d"SCNd32,&n);
+    printf("%"PRId32"\n",factorial(n));
+    return 0;
+}
+
+int32_t factorial(int32_t n)
+{
+    int32_t res = n;
+    if (n>1) res *= factorial(n-1);
+    return res;
+}

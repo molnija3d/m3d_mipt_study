0b9a8386630f801d8357276863c2a82a4cf248ee
Molnija3D <molnija3d@yandex.ru>
2024-04-23 21:01:47 +0300

    Добавить C18.c

---
+++ HW06/C18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <inttypes.h>
+/*
+Составить логическую функцию, которая определяет, что текущий символ это цифра. Написать программу считающую количество цифр в тексте.
+int is_digit(char c)
+ */
+
+int32_t is_digit(char c);
+
+
+int main(void)
+{
+    char c;
+    int32_t i=0;
+    while((c=getchar())!='.')
+    {
+     if(is_digit(c)) 
+       ++i;
+    }
+    printf("%"PRId32"\n",i);
+    return 0;
+}
+
+    
+int32_t is_digit(char c)
+{
+  if(c >= 0x30 && c <= 0x39) return 1;
+  return 0;  
+}

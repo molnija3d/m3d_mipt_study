f2e54babb81e1b79e84c46a2ab48d14ef63b2f70
Molnija3D <molnija3d@yandex.ru>
2024-04-27 22:20:16 +0300

    Добавить D20.c

---
+++ HW07/D20.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <inttypes.h>
+/*
+Написать рекурсивную функцию возведения целого числа n в степень p.
+int recurs_power(int n, int p)
+Используя данную функцию, решить задачу.
+*/
+int recurs_power(int n, int p);
+
+int main(void)
+{
+    int n,p;
+    scanf("%d %d",&n,&p);
+    printf("%d\n",recurs_power(n,p));
+    return 0;
+}
+
+int recurs_power(int n, int p)
+{
+    if(p == 0)
+        return 1;
+    if(p>1)
+        n *= recurs_power(n, p-1);
+    return n;
+}

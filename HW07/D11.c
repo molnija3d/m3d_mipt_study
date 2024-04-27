b0f9826ff3a83f17ee82f917e6ff8bc37d8b9f5f
Molnija3D <molnija3d@yandex.ru>
2024-04-27 11:08:18 +0300

    Добавить D11.c

---
+++ HW07/D11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <inttypes.h>
+/*
+Дано натуральное число N. Посчитать количество «1» в двоичной записи числа. Составить рекурсивную функцию.
+*/
+int32_t r_sum1(int32_t n);
+
+int32_t main(void)
+{
+    int n;
+    scanf("%d",&n);
+    printf("%"PRId32"\n",r_sum1(n));
+    return 0;
+}
+
+int32_t r_sum1(int32_t n)
+{
+    static int32_t sum=0;
+    if(n)
+    {
+        sum = r_sum1(n/2);
+        if(n%2)
+            return ++sum;
+    }
+    return sum;
+}

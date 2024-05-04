c2cb3c5de4b79d3b1e67ff554d8e0cb1ad8ddfa9
Molnija3D <molnija3d@yandex.ru>
2024-05-04 10:49:10 +0300

    Добавить E02.c

---
+++ HW08/E02.c
@@ -0,0 +1,36 @@
+/*
+Найти минимум
+Ввести c клавиатуры массив из 5 элементов, найти минимальный из них.
+Input format
+5 целых чисел через пробел
+Output format
+Одно единственное целое число
+*/
+#include <stdio.h>
+#define SIZE 5
+
+void input(int *arr);
+int min(int *arr);
+
+int main(void)
+{
+    int arr[SIZE] = {0};
+    input(arr);
+    printf("%d\n",min(arr));
+    return 0;
+}
+
+void input(int *arr)
+{
+    for(int i=0; i<SIZE; i++)
+        scanf("%d",arr+i);
+}
+
+int min(int *arr)
+{
+    int min = arr[0];
+    for(int i=1; i<SIZE; i++)
+        if(min > arr[i])
+            min=arr[i];
+    return min;
+}

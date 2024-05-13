13f94d6ad7c2df8a1502fec2f1a43766195dd025
Molnija3D <molnija3d@yandex.ru>
2024-05-13 14:33:29 +0300

    Оформление F02.c

--- HW09/F02.c
+++ HW09/F02.c
@@ -8,25 +8,6 @@ Input format
 Output format
 Отсортированный исходный массив
 */
-/*
-#include <stdio.h>
-#include <inttypes.h>
-#include <stdlib.h>
-#include <time.h>
-
-void inparr(int32_t* arr,int32_t n);
-void printarr(int32_t* arr,int32_t n);;
-void sort_even_odd(int n, int a[]);
-int main(void)
-{
-    int a[20]={0};
-    inparr(a,20);
-    printarr(a,20);
-    sort_even_odd(20,a);
-    printarr(a,20);
-    return 0;
-}
-*/
 void sort_even_odd(int n, int a[])
 {
     int buff[n],cnt=0;
@@ -41,18 +22,3 @@ void sort_even_odd(int n, int a[])
     for(int i=0; i<n; i++)
       a[i]=buff[i];
 }
-/*
-void inparr(int32_t* arr,int32_t n)
-{
-    srand(time(NULL));
-    for(int i = 0; i < n ; i++)
-        arr[i]=rand()%100;
-}
-void printarr(int32_t* arr,int32_t n)
-{
-    for (int i = 0; i < n; i++)
-        printf("%3d",arr[i]);
-        
-    printf("\n");
-}
-*/

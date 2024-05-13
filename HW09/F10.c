93666aa710ecc706ca144dc1127133d8265f858c
Molnija3D <molnija3d@yandex.ru>
2024-05-13 11:48:58 +0300

    Добавить F10.c

---
+++ HW09/F10.c
@@ -0,0 +1,39 @@
+/*
+Упаковать строку
+Дана строка состоящая из маленьких латинских букв 'a'..'z'. В конце строки точка. 
+Необходимо заменить повторяющиеся буквы на <буква><количество повторений>
+Input format
+Входная строка состоящая из букв 'a'-'z' не превышающая 1000 символов. В конце строки символ '.'
+Output format
+Результат состоящий из букв и чисел, без пробелов
+*/
+#include <stdio.h>
+
+void archStr(void);
+
+int main(void)
+{
+    archStr();
+    return 0;
+}
+
+void archStr(void)
+{
+    char c,prev;
+    int cnt=0;
+    prev=getchar();
+    do
+     {
+      c=getchar();
+      if(c == prev)
+       ++cnt;
+      else
+      {
+       printf("%c%d",prev,cnt+1);
+       prev=c;
+       cnt=0;
+      }
+     }
+    while(c!='.'); 
+}
+

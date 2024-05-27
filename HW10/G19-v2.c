9aeabf7165dae2f1a4c7275109b0805e6e3fb1f7
Molnija3D <molnija3d@yandex.ru>
2024-05-27 08:58:42 +0300

    Добавить частотный анализ для G19=

--- HW10/G19-v2.c
+++ HW10/G19-v2.c
@@ -8,119 +8,77 @@ Output format
 Строка из маленьких английских букв.
  */
 #include <stdio.h>
-
-int strinp(FILE *inp, char* str);
-void sort_str(int size, char* a);
+int strinp(FILE *inp, char *str);
 void make_palindrom(int in_len, char *in_str, char *plndr);
 
 int main(void)
 {
-    enum {str_len = 1001};
-    FILE *inp = fopen("input.txt","r");
-    FILE *outp = fopen("output.txt","w");
-    char in_str[str_len]= {'\0'};
+    enum
+    {
+        str_len = 1001
+    };
+    FILE *inp = fopen("input.txt", "r");
+    FILE *outp = fopen("output.txt", "w");
+
+    char in_str[str_len] = {'\0'};
     char palindrom[str_len] = {'\0'};
     int len = 0;
+
     len = strinp(inp, in_str);
-    sort_str(len, in_str);
-    make_palindrom(len, in_str, palindrom);
-
-    fprintf(outp, "%s", palindrom);
-
     fclose(inp);
-    fclose(outp);
 
+    make_palindrom(len, in_str, palindrom);
+    fprintf(outp, "%s", palindrom);
+    
+    fclose(outp);
     return 0;
 }
 void make_palindrom(int in_len, char *in_str, char *plndr)
 {
-    int e_idx = 0, s_idx = 0;
-    char spare[33] = {'\0'};
-    char even[1001] = {'\0'};
-    for(int i = 1; i <= in_len; i++)
+    char frq_lttr[26] = {0}, pivot = 0;
+    int idx = 0, l_lngth;
+
+    for (int i = 0; i < in_len; ++i)
     {
-        if(in_str[i-1] == in_str[i])
+        ++frq_lttr[in_str[i] - 'a'];
+    }
+
+    for (int i = 0; i < 26; ++i)
+    {
+        if ((frq_lttr[i] % 2) && (pivot == 0))
         {
-            even[e_idx++] = in_str[i-1];
-            even[e_idx++] = in_str[i++];
+            pivot = i + 'a';
         }
-        else
+        for (int j = 0; j < frq_lttr[i] / 2; ++j)
         {
-            spare[s_idx++] = in_str[i-1];
+            plndr[idx++] = i + 'a';
         }
     }
 
-    int lft_idx = e_idx + 1 * (s_idx > 0);
-    char left[lft_idx];
-    _Bool flag = 1;
-    int pldr_lidx = 0;
-    
-    for(int i = 0; i < lft_idx && flag; i++)
+    l_lngth = idx;
+
+    if (pivot)
     {
-        if((i+i) < e_idx)
-        {
-            left[i] = even[i+i];
-            pldr_lidx = i;
-        }
-        else if (s_idx > 0)
-        {
-            left[i] = spare[0];
-            flag = 0;
-            pldr_lidx = i + 1;
-        }
+        plndr[idx] = pivot;
     }
 
-    if(pldr_lidx % 2 != 0)
+    for(int i = 0; i <= l_lngth; ++i)
     {
-        for(int i = 0; i < pldr_lidx - 1; i++)
-        {
-            plndr[i] = left[i];
-            plndr[2 * pldr_lidx - 2 - i] = plndr[i];
-        }
-        plndr[pldr_lidx - 1] = left[pldr_lidx - 1];
+       plndr[idx++] = plndr[l_lngth - i];
     }
-    else
-    {
-        for(int i = 0; i < pldr_lidx; i++)
-        {
-            plndr[i] = left[i];
-            plndr[2 * pldr_lidx - 2 - i] = plndr[i];
-        }
-    }
-    plndr[2 * pldr_lidx - 1] = '\0';
 }
 
-
-int strinp(FILE *inp, char* str)
+int strinp(FILE *inp, char *str)
 {
     int i = 0;
     char c = 0;
-    while((c = fgetc(inp)) != EOF)
+    while ((c = fgetc(inp)) != EOF)
     {
-        if((c >= 'a' && c <= 'z')) //in auto test there are only small letters
+        if ((c >= 'a' && c <= 'z')) // in auto test there are only small letters
         {
             str[i++] = c;
         }
     }
     str[i] = '\0';
     return i;
-}
-
-void sort_str(int size, char* a)    //shell-sort
-{
-    for (int stp = size / 2; stp > 0; stp /= 2)
-    {
-        for (int i = stp; i < size; i++)
-        {
-            int j = i;
-            int temp = a[j]; //remember number at postition stp
-
-            while( j - stp >= 0 && a[j - stp] > temp)   //if val at position j-stp > temp, change val at j with val at j-stp
-            {
-                a[j] = a[j - stp];
-                j -= stp;
-            }
-            a[j] = temp; //place data[i] at new position.
-        }
-    }
 }

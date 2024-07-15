23c1beb3be8bc6b0d9df9c5c2a2b8382d5b74205
molnija3d <molnija3d@yandex.ru>
2024-07-15 15:58:42 +0300

    add hw01-08

---
+++ Advanced_C/HW01/hw01-08.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+
+int main()
+{
+        uint32_t n = 0;
+        int32_t a = 0, res = 0;
+        scanf("%" SCNu32, &n);
+        scanf("%" SCNd32, &res);
+
+        while(--n)
+        {
+         scanf("%" SCNd32, &a);
+         res ^= a;
+        }
+        printf("%" PRId32 "\n", res);
+        return 0;
+}

c88eb87e1f2d3cd190ce50d0780d9b88e6cc2162
molnija3d <molnija3d@yandex.ru>
2024-07-15 16:25:23 +0300

    add hw01-11

---
+++ Advanced_C/HW01/hw01-11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int extractExp(float f)
+{
+        union 
+        {
+           float val;
+           struct 
+           {
+              uint32_t m: 23;
+              uint32_t exp: 8;
+              uint32_t s: 1;
+           } bit;
+        } in;
+        in.val = f;
+        return in.bit.exp;
+}
+
+int main()
+{
+        float f;
+        scanf("%f", &f);
+        printf("%d\n", extractExp(f));
+        return 0;
+}

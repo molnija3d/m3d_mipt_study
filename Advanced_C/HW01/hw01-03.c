facaa3baa9a9015996deccb094ba1d20dba94796
molnija3d <molnija3d@yandex.ru>
2024-07-15 12:23:43 +0300

    Add hw03

---
+++ Advanced_C/HW01/hw01-03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+
+uint32_t rsht(uint32_t n, uint32_t rot)
+{
+        return ( n >> rot ) | ( n << (32 - rot));
+}
+
+int main(void)
+{
+        uint32_t n, rot;
+        scanf("%" SCNu32"%" SCNu32, &n, &rot);
+        printf("%" PRIu32"\n", rsht(n, rot));
+        return 0;
+}
+
+

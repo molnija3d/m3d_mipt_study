c9de59f38c44ad057d784baab32e7ff1a219d715
Molnija3D <molnija3d@yandex.ru>
2025-01-19 15:45:24 +0300

    float

--- MC_Intro/intro_hw01-2.c
+++ MC_Intro/intro_hw01-2.c
@@ -5,10 +5,41 @@ int main(void) {
     float a = 0;
     printf("Enter float value:");
     scanf("%f", &a);
-    uint32_t m, e, bits = *(uint32_t *)&a;
-    e = (bits >> 23) & 0xff;
-    m  = bits & 0x7fffff ;
-    printf("m = %d, e = %d k = %d\n", m, e, bits);
+    int32_t e;
+    uint32_t bits = *(uint32_t *)&a;
 
+    e = ((bits >> 23) & 0xff) - 127;
+    float m  = (float) (bits & 0x7fffff) / 0x800000 ;
+    if(bits >> 31) {
+        putchar('-');
+    }
+    putchar('1');
+    putchar(',');
+
+    for(int i = 0; i < 9; i++) {
+        m *= 10;
+	putchar('0' + m);
+	m -= (int) m;
+    }
+    
+    putchar('*');
+    putchar('2');
+    putchar('^');
+
+    if(e < 0){
+	    putchar('-');
+	    e = -e;
+    }
+    
+    if(e > 99){
+	    putchar('1');
+	    e %= 100;
+    }
+    if(e > 9){
+	    putchar('1');
+	    e %= 10;
+    }
+    putchar('0' + e);
     return 0;
+
 }
